c1d8becf96817a356b113e6429c45b1f68d0d19d
adithep.b <adithepbaebmueann@gmail.com>
2025-07-11 22:10:15 +0700

    delete unnecessary function

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -3,11 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root() -> dict:
-    return {"Hello": "World"}
-
-
 @app.get("cases/{case_id}")
 def read_item(item_id: int, q: str | None = None) -> dict:
     return {"item_id": item_id, "q": q}
