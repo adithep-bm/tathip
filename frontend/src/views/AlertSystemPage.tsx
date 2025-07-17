4f971abafdc20c07110dcdf6ea38001de31114d2
Papanya Siriwattanaworakul <papanya2546@gmail.com>
2025-07-17 09:30:58 +0700

    Alert System
    fill Header and split components

--- frontend/src/views/AlertSystemPage.tsx
+++ frontend/src/views/AlertSystemPage.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
+import HeaderAlertSystem from "../components/AlertSystem/HeaderAlertSystem";
 import Header from "../components/Header";
 import SideBar from "../components/SideBar";
+import type { Alert } from "../types/alert";
 
 function AlertSystemPage() {
+
+  const [alerts, setAlerts] = useState<Alert[]>([
+    {
+      id: '1',
+      type: 'high',
+      title: 'ตรวจพบการโอนเงินต้องสงสัย',
+      message: 'พบการโอนเงินจำนวน 500,000 บาท ไปยังบัญชีที่อยู่ใน watchlist',
+      timestamp: '2024-01-16 16:45:00',
+      status: 'unread',
+      source: 'OCR System'
+    }
+  ]);
   return (
     <div className="min-h-screen bg-slate-900">
       {/* Header */}
@@ -12,6 +28,7 @@ function AlertSystemPage() {
           {/* Main Content */}
           <div className="lg:col-span-3 space-y-6">
             <div className="space-y-6">
+              <HeaderAlertSystem alerts={alerts} />
             </div>
           </div>
         </div>
